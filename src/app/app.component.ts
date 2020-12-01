dfddf9bf715943b9535eadf03e80a61edbf1762d
pablo <ppitacaneiro@gmail.com>
2020-12-01 20:10:41 +0100

    Implementado slideshow en la home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { PeliculasService } from './services/peliculas.service';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +8,6 @@ import { PeliculasService } from './services/peliculas.service';
 export class AppComponent {
   title = 'PeliculasApp';
 
-  constructor(private peliculasService:PeliculasService) {
-    this.peliculasService.getCartelera().subscribe(reponse => console.log(reponse));
+  constructor() {
   }
 }
