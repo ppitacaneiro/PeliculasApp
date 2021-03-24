033e85baeb728bda1c1acd0c90e228f8d7a297fb
pablo <ppitacaneiro@gmail.com>
2021-03-24 20:34:07 +0100

    Detalle de pelicula

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,12 +4,16 @@ import { HomeComponent } from './home/home.component';
 import { PeliculaComponent } from './pelicula/pelicula.component';
 import { BuscarComponent } from './buscar/buscar.component';
 import { ComponentsModule } from '../components/components.module';
+import { PipesModule } from '../pipes/pipes.module';
+import { RatingModule } from 'ng-starrating';
 
 @NgModule({
   declarations: [HomeComponent, PeliculaComponent, BuscarComponent],
   imports: [
     CommonModule,
-    ComponentsModule
+    ComponentsModule,
+    PipesModule,
+    RatingModule
   ]
 })
 export class PagesModule { }
--- src/app/pages/pelicula/pelicula.component.ts
+++ src/app/pages/pelicula/pelicula.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { MovieDetail } from 'src/app/interfaces/movie-detail';
 import { PeliculasService } from 'src/app/services/peliculas.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-pelicula',
@@ -9,14 +11,23 @@ import { PeliculasService } from 'src/app/services/peliculas.service';
 })
 export class PeliculaComponent implements OnInit {
 
-  constructor(private activatedRouter:ActivatedRoute, private peliculaService:PeliculasService) { }
+  movie:MovieDetail;
+
+  constructor(private activatedRouter:ActivatedRoute,
+              private peliculaService:PeliculasService,
+              private location:Location) { }
 
   ngOnInit(): void {
     const idMovie = this.activatedRouter.snapshot.params.id;
     console.log('idMovie => ' + idMovie);
     this.peliculaService.getMovieDetail(idMovie).subscribe(movie => {
-      console.log(movie);
+      // console.log(movie);
+      this.movie = movie;
     });
+  }
+
+  goBack() {
+    this.location.back();
   }
 
 }
